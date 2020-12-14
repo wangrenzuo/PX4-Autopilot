6a3cf6f5c32fa606806111ee30bcfba5878b2fd6
Peter van der Perk <peter.vanderperk@nxp.com>
2020-12-14 21:49:03 +0100

    FMUK66 NuttX 10 SocketCAN initialization

--- boards/nxp/fmuk66-v3/src/init.c
+++ boards/nxp/fmuk66-v3/src/init.c
@@ -293,6 +293,14 @@ __EXPORT int board_app_initialize(uintptr_t arg)
 	kinetis_netinitialize(0);
 # endif
 
+# ifdef CONFIG_KINETIS_FLEXCAN0
+	kinetis_caninitialize(0);
+# endif
+
+# ifdef CONFIG_KINETIS_FLEXCAN1
+	kinetis_caninitialize(1);
+# endif
+
 #endif
 
 	return OK;
