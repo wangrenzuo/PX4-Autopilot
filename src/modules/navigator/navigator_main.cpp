e8271fcf959f7fffc3d1c4d3cf39589219147851
Daniel Agar <daniel@agar.ca>
2021-01-31 15:59:53 -0500

    WIP: move follow to flight_task

--- src/modules/navigator/navigator_main.cpp
+++ src/modules/navigator/navigator_main.cpp
@@ -83,8 +83,7 @@ Navigator::Navigator() :
 	_precland(this),
 	_rtl(this),
 	_engineFailure(this),
-	_gpsFailure(this),
-	_follow_target(this)
+	_gpsFailure(this)
 {
 	/* Create a list of our possible navigation types */
 	_navigation_mode_array[0] = &_mission;
@@ -95,7 +94,6 @@ Navigator::Navigator() :
 	_navigation_mode_array[5] = &_takeoff;
 	_navigation_mode_array[6] = &_land;
 	_navigation_mode_array[7] = &_precland;
-	_navigation_mode_array[8] = &_follow_target;
 
 	_handle_back_trans_dec_mss = param_find("VT_B_DEC_MSS");
 	_handle_reverse_delay = param_find("VT_B_REV_DEL");
@@ -622,11 +620,6 @@ Navigator::run()
 		case vehicle_status_s::NAVIGATION_STATE_AUTO_LANDGPSFAIL:
 			_pos_sp_triplet_published_invalid_once = false;
 			navigation_mode_new = &_gpsFailure;
-			break;
-
-		case vehicle_status_s::NAVIGATION_STATE_AUTO_FOLLOW_TARGET:
-			_pos_sp_triplet_published_invalid_once = false;
-			navigation_mode_new = &_follow_target;
 			break;
 
 		case vehicle_status_s::NAVIGATION_STATE_MANUAL:
